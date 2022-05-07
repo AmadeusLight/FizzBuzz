1fdcbd9d8f49c5cdf3675cc12f63a638520030d1
AmadeusLight <70249580+AmadeusLight@users.noreply.github.com>
2022-05-07 11:45:36 -0700

    okasdokasdokas

---
+++ test/services/FizzbuzzService.test.js
@@ -0,0 +1,14 @@
+const ExplorerService = require("../../lib/services/ExplorerService");
+
+describe("Unit Tests for ExplorerService Class", () => {
+    test('1) Filter Explorers By Mission', () => {
+
+      const explorers = [{mission:"node"}]
+
+      const explorersInNode = ExplorerService.filterByMision(explorers,"node");
+
+      expect(explorersInNode.length).toBe(1)
+
+
+    });
+  })
---
+++ test/utils/Reader.test.js
@@ -0,0 +1,12 @@
+const Reader = require("../../lib/utils/Reader");
+
+describe("Unit Tests for Reader Class", () => {
+    test('1) Read explorers.json', () => {
+
+      const explorers = Reader.readJsonFile("test/data/explorers_test_file_1.json");
+
+      expect(explorers).not.toBeUndefined()
+
+
+    });
+  })
